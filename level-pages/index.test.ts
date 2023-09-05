f1cf4b886ed2124d1f8f3b897841870094e0d0a6
AnnaIllarionova <annailinas@gmail.com>
2023-09-05 20:44:40 +0300

    added tests for timer

--- level-pages/index.test.ts
+++ level-pages/index.test.ts
@@ -3,7 +3,8 @@
  */
 import { describe, expect, test } from "@jest/globals";
 import { shuffleCardsArray } from "./shuffled-function";
-// import { minutes, useTimer } from "./timer";
+import { useLogicOfTimer, minutes, seconds, resetTimer } from "./timer";
+import { interval } from "./render-game-field";
 
 describe("shuffleCardsArray", () => {
     test("the original array is not equal to the shuffled one", () => {
@@ -15,5 +16,26 @@ describe("shuffleCardsArray", () => {
         const originalArray = [...array];
         const shuffledArray = shuffleCardsArray(array);
         expect(shuffledArray).not.toBe(originalArray);
+    });
+});
+
+describe("useLogicOfTimer", () => {
+    beforeEach(() => {
+        resetTimer(interval);
+    });
+
+    test("when seconds are 60 the value of minutes changes to 1 ", () => {
+        for (let i = 0; i < 60; i++) {
+            useLogicOfTimer();
+        }
+        useLogicOfTimer();
+        expect(minutes).toEqual(1);
+    });
+
+    test("when seconds are 60 the value of seconds changes to 0 ", () => {
+        for (let i = 0; i < 60; i++) {
+            useLogicOfTimer();
+        }
+        expect(seconds).toEqual(0);
     });
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,9 @@
-// const path = require("path");
-// const HtmlWebpackPlugin = require("html-webpack-plugin");
-// const CopyPlugin = require("copy-webpack-plugin");
-// const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-// const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-import path from "path";
-import HtmlWebpackPlugin from "html-webpack-plugin";
-import CopyPlugin from "copy-webpack-plugin";
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
-import CssMinimizerPlugin from "css-minimizer-webpack-plugin";
+/* eslint @typescript-eslint/no-var-requires: "off" */
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 const isProduction = process.env.NODE_ENV === "production";
 
